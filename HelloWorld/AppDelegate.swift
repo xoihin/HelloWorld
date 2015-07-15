c558ade35e1f0518f411aba51752a69bd1a41154
xoihin <hinxoi@hotmail.com>
2015-07-14 22:12:59 -0400

    HelloWorld - done

--- HelloWorld/AppDelegate.swift
+++ HelloWorld/AppDelegate.swift
@@ -17,10 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         
-        // NSLog(@"Documents folder: %@",[[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject]);
-        
-        // line 2
-        
+                
         return true
     }
 
--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    
+    @IBOutlet weak var lblOutput: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        lblOutput.text = "Bonjour!"
+        
     }
 
     override func didReceiveMemoryWarning() {
